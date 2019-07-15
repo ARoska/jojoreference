759efdb98db8995998df640a68acf1dd9546f1c8
Andrew Roska <andrew.l.roska@gmail.com>
2019-07-14 22:19:17 -0700

    Init.

---
+++ JojoRef/Data/JojoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JojoRef.Data
+{
+    public class JojoDbContext : DbContext
+    {
+        public JojoDbContext(DbContextOptions<JojoDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ JojoRef/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JojoRef.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public static class ApplicationRoles
+    {
+        public const string Member = "Member";
+        public const string Admin = "Admin";
+    }
+}
